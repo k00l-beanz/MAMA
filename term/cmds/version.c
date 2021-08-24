3dd6439b0f6fa5b26e8edc95ff7ec66e5b34721a
root <root@loud-20.04>
2021-08-24 18:54:18 -0400

    version.c

--- term/cmds/version.c
+++ term/cmds/version.c
@@ -6,8 +6,10 @@
 */
 int cmd_version(char *arg_str) {
 	(void)arg_str;
-
-	println("You're inside the handler function for the 'help' command!", 58);
-	println("Someone should implement me :(", 30);
+	
+	// printing the OS version number
+	char *versionOs= "\nCurrent Version: R1\n";
+	println(versionOs,20);
+	
 	return 0;
 }
