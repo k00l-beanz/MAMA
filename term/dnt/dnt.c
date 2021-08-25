a5e746cea9a6a409c25fb0201e44bb3c71bcafc0
as10 <as10@loud-20.04>
2021-08-25 10:51:28 -0400

    Finished syntax highlighting

--- term/dnt/dnt.c
+++ term/dnt/dnt.c
@@ -272,6 +272,7 @@ char * intToMonth(int value) {
       break;
     case 11:
       month = "December";
+      break;
     default:
       month = "";
   }
--- term/visuals/syntax.c
+++ term/visuals/syntax.c
@@ -6,6 +6,7 @@
 
 enum SyntaxState states[MAX_SYNTAX_SWITCHES];
 int switch_indexes[MAX_SYNTAX_SWITCHES];
+int newest_switch;
 
 void switch_to(enum SyntaxState, int, int);
 void color_for(enum SyntaxState);
@@ -14,6 +15,7 @@ void get_state_at(int, int *);
 void syntax_init() {
 	states[0] = CMD_NAME;
 	switch_indexes[0] = 0;
+	newest_switch = 0;
 	int i; // >:(
 	for(i = 1; i < MAX_SYNTAX_SWITCHES; i++) {
 		states[i] = DEFAULT;
@@ -22,7 +24,7 @@ void syntax_init() {
 }
 
 void syntax_handle_char(char c, int index) {
-	return; /* TODO: finish syntax highlighting */
+	// return; /* TODO: finish syntax highlighting */
 
 	int record_index;
 	get_state_at(index, &record_index);
@@ -38,10 +40,22 @@ void syntax_handle_char(char c, int index) {
 				switch_to(DOUBLE_QUOTE_STRING_END_QUOTE, index, record_index);
 				break;
 			case SINGLE_QUOTE_STRING:
-				switch_to(SINGLE_QUOTE_STRING_END_QUOTE, index, record_index);
+				color_for(cur_state);
 				break;
 			default:
 				switch_to(DOUBLE_QUOTE_STRING, index, record_index);
+				break;
+		}
+	} else if(c == '\'') {
+		switch(cur_state) {
+			case SINGLE_QUOTE_STRING:
+				switch_to(SINGLE_QUOTE_STRING_END_QUOTE, index, record_index);
+				break;
+			case DOUBLE_QUOTE_STRING:
+				color_for(cur_state);
+				break;
+			default:
+				switch_to(SINGLE_QUOTE_STRING, index, record_index);
 				break;
 		}
 	} else if(c == '-') {
@@ -83,10 +97,13 @@ void syntax_handle_char(char c, int index) {
 }
 
 void switch_to(enum SyntaxState state, int index, int record_index) {
-	record_index++;
-	states[record_index] = state;
-	color_for(state);
-	switch_indexes[record_index] = index;
+	if(record_index + 1 < MAX_SYNTAX_SWITCHES) {
+		record_index++;
+		states[record_index] = state;
+		color_for(state);
+		switch_indexes[record_index] = index;
+		newest_switch = record_index;
+	}
 }
 
 void color_for(enum SyntaxState state) {
@@ -101,6 +118,7 @@ void color_for(enum SyntaxState state) {
 			break;
 		case PARAM_NAME:
 			display_fg_color(SYNTAX_COLOR_PARAM_NAME);
+			//display_italicize();
 			break;
 		case PARAM_VALUE:
 			display_fg_color(SYNTAX_COLOR_PARAM_VALUE);
@@ -118,7 +136,7 @@ void color_for(enum SyntaxState state) {
 void get_state_at(int index, int *index_of_state_in_record) {
 	int i;
 	*index_of_state_in_record = 0;
-	for(i = 0; i < MAX_SYNTAX_SWITCHES; i++) {
+	for(i = 0; i <= newest_switch; i++) {
 		if(switch_indexes[i] > index)
 			break;
 		*index_of_state_in_record = i;
